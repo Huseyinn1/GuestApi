88d5eb90ca80c7f70c347b8a1deee750acd0a033
Hüseyin Adıgüzel <ahuseyin235@gmail.com>
2023-06-03 22:47:34 +0300

    Added Repository Manager

---
+++ Repositories/Core/RepositoryManager.cs
@@ -0,0 +1,28 @@
+﻿using Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Core
+{
+    public class RepositoryManager : IRepositoryManager
+    {
+
+
+        private readonly RepositoryContext _context;
+
+        public RepositoryManager(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        public IGuestRepository Guest => new GuestRepository(_context);
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
